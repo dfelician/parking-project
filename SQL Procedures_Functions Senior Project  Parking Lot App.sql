6e19e34b3f7276a58c51efdcdbbd7ee4af10ba99
artiesinger7 <56160624+artiesinger7@users.noreply.github.com>
2022-10-26 22:55:35 -0400

    Update SQL Procedures_Functions Senior Project  Parking Lot App.sql

--- SQL Procedures_Functions Senior Project  Parking Lot App.sql
+++ SQL Procedures_Functions Senior Project  Parking Lot App.sql
@@ -1,9 +1,14 @@
-exec updateLot01Proc -- Updates lot01 sets available to 'Y', username = NULL, ramID NULL. Runs at midnight with the query: 'IF(CONVERT(VARCHAR(8),GETDATE(),108) LIKE '00:00%')'. Neccessary to reset lots every night. Program automatically will run at midnight using Windows Scheduler. 
+/*
+Runs at midnight with the query: 'IF(CONVERT(VARCHAR(8),GETDATE(),108) LIKE '00:00%')'. Neccessary to reset lots every night. 
+Program automatically will run at midnight using Windows Scheduler. 
+*/
 
 
+--Updates lot01 sets available to 'Y', username = NULL, ramID NULL. 
+exec updateLot01Proc
 
 --Syntax of Proc
-/*
+
 /****** Object:  StoredProcedure [dbo].[updateLot01Proc]    Script Date: 12/17/2019 4:19:49 PM ******/
 SET ANSI_NULLS ON
 GO
@@ -21,20 +26,13 @@ update lot01 set Available = 'Y',RamID = 'NULL',UserName = 'NULL' where SpotNumb
 BEGIN
 	print 'Lot01 Not Updated'
 	END
-*/
-
-
-
-
-
-
-
-
-exec updateLot02Proc --Updates lot02 sets available to 'Y', username = NULL, ramID NULL.Runs at midnight with the query: 'IF(CONVERT(VARCHAR(8),GETDATE(),108) LIKE '00:00%')'. Neccessary to reset lots every night. Program automatically will run at midnight using Windows Scheduler. 
-
+	
+	
+--Updates lot02 sets available to 'Y', username = NULL, ramID NULL.
+exec updateLot02Proc 
 
 --Syntax of Proc
-/*
+
 /****** Object:  StoredProcedure [dbo].[updateLot02Proc]    Script Date: 12/17/2019 4:19:57 PM ******/
 SET ANSI_NULLS ON
 GO
@@ -52,18 +50,14 @@ update lot02 set Available = 'Y',RamID = 'NULL',UserName = 'NULL' where SpotNumb
 BEGIN
 	print 'Lot02 Not Updated'
 	END
-*/
 
 
 
-
-
-
-exec updateLot03Proc --Updates lot03 sets available to 'Y', username = NULL, ramID NULL.Runs at midnight with the query: 'IF(CONVERT(VARCHAR(8),GETDATE(),108) LIKE '00:00%')'. Neccessary to reset lots every night. Program automatically will run at midnight using Windows Scheduler. 
-
+ --Updates lot03 sets available to 'Y', username = NULL, ramID NULL.
+exec updateLot03Proc
 
 --Syntax of Proc
-/*
+
 /****** Object:  StoredProcedure [dbo].[updateLot03Proc]    Script Date: 12/17/2019 4:20:05 PM ******/
 SET ANSI_NULLS ON
 GO
@@ -81,19 +75,17 @@ ELSE
 BEGIN
 	print 'Lot03 Not Updated'
 	END
-*/
 
 
 
-
-
-
-
-
-
-exec AdminReserveUpdate --Updates course to set start_time equal to the current time, in order to get around the CK_Lot01 check constraint. Mainly used as a nested proc.
---Syntax of Proc
 /*
+Updates course to set start_time equal to the current time, in order to get around the CK_Lot01 check constraint. 
+Mainly used as a nested proc.
+*/
+exec AdminReserveUpdate 
+
+--Syntax of Proc
+
 /****** Object:  StoredProcedure [dbo].[AdminReserveUpdate]    Script Date: 12/17/2019 4:18:09 PM ******/
 SET ANSI_NULLS ON
 GO
@@ -105,15 +97,18 @@ DECLARE @SpotNumber int
 update course set start_time = (convert(VARCHAR, DATEADD(MINUTE, -300, GETDATE()), 100)) where CRN = '55555'
 update Lot01 set Available = 'N', RamID = 'ADMIN' where SpotNumber = @SpotNumber
 
+
+
+
+
+/*
+Procedure used for reserving multiple spots if the date in dbo.PendingRequests is equal to the current date (GETDATE()). 
+Will reserve the number of spots ranging from 1 to x depending on the number of spots reserved in dbo.PendingRequests
 */
-
-
-
-
-exec MorningAdminProcedure --Procedure used for reserving multiple spots if the date in dbo.PendingRequests is equal to the current date (GETDATE()). Will reserve the number of spots ranging from 1 to x depending on the number of spots reserved in dbo.PendingRequests
+exec MorningAdminProcedure s
 
 --Syntax of Proc
-/*
+
 /****** Object:  StoredProcedure [dbo].[MorningAdminProcedure]    Script Date: 12/17/2019 4:17:16 PM ******/
 SET ANSI_NULLS ON
 GO
@@ -128,19 +123,22 @@ BEGIN
 update lot01 set Available = 'N',RamID = 'ADMIN',UserName = 'ADMINTEST' where SpotNumber BETWEEN 1 AND (Select numSpots from PendingRequests where dateOfEvent = (convert(VARCHAR, DATEADD(MINUTE, -300, GETDATE()), 100)))        print 'Lot01 Updated'
 END
 END
-*/
+
 
 --FUNCTION
 
 
-exec dbo.Lot01CheckCsontraintFunction @RamID = 'R01632755'
+exec dbo.Lot01CheckConstraintFunction @RamID = 'R01532354'
 
-Function mainly used for the Lot01 check constraint. If this function returns true, allow a user to reserve a spot in lot01. This function checks to see if a student has a class within 15 minutes before the class starts, for that specific @RamID
-
+/*
+Function mainly used for the Lot01 check constraint. 
+If this function returns true, allow a user to reserve a spot in lot01. 
+This function checks to see if a student has a class within 15 minutes before the class starts, for that specific @RamID
+*/
 
 --Syntax of Function:
 
-/*
+
 /****** Object:  UserDefinedFunction [dbo].[Lot01CheckConstraintFunction]    Script Date: 12/17/2019 4:24:11 PM ******/
 SET ANSI_NULLS ON
 GO
@@ -170,5 +168,3 @@ AND (convert(VARCHAR, DATEADD(MINUTE, -285, GETDATE()), 100)))
         return 'True'
     return 'False'
 END
-*/
-@ToyinIku#3450  Since i never put any database/SQL things on github. @here if you want to reference them later, or required for resume
